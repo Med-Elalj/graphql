f58beda7f7906e13de148113419e037855c48aa9
Med-Elalj <MohammedElalj@outlook.com>
2025-05-16 22:12:27 +0100

    done

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function logout() {
     localStorage.removeItem('token');
     location.reload();
 }
+
 function calcCoordinates(Nsides, radius) {
     const angle = Math.PI / (Nsides / 2);
     let coord = [];
@@ -22,22 +23,18 @@ function calcCoordinates(Nsides, radius) {
 // Function to update the SVG polygon based on skill values
 function renderGraph(skills) {
     let res = calcCoordinates(skills.length, 60).map(item => {
-        console.log("line", item);
         return `<line x1="0" y1="0" x2="${item.x}" y2="${item.y}" stroke="#cce" stroke-width="2" />\n`;
     }).join(' ');
     let coord = [20, 40, 60, 80, 100].forEach(r => {
         let points = calcCoordinates(skills.length, 60 * r / 100).map(item => `${item.x},${item.y}`).join(' ');
-        console.log("r", r, points)
         res += `<polygon class="polygon" points="${points}" stroke="#ccc" fill="none"/>\n`
     })
 
     const polygon = document.getElementById("skill-polygon");
     const labelcoord = calcCoordinates(skills.length, 78)
-    console.log(labelcoord)
     let last = '';
     const points = skills.map((skill, index) => {
         let c = calcCoordinates(skills.length, 60 * skill.level / 100)[index];
-        console.log("skill", skill,index,skill.name,skill.level,`<title>${skill.name} : ${skill.level}</title>`)
         last += `<circle onhover="console.log("test")" cx="${c.x}" cy="${c.y}" r="3" fill="red" ><title>${skill.name} : ${skill.level}</title></circle>\n`;
         last += `<text x="${labelcoord[index].x}" y="${labelcoord[index].y}" text-anchor="middle" fill="red" dominant-baseline="central"  font-size="9px"><title>${skill.name} : ${skill.level}</title>${skill.name}</text>\n`
         return `${c.x},${c.y}`;
@@ -49,12 +46,46 @@ function renderGraph(skills) {
         </svg>`
 }
 
+function module(projects) {
+    // { xp: 5000, project: "go-reloaded", createdAt: "2024-04-29T12:19:33.931771+00:00" }
+    const start = new Date(projects[0].createdAt)
+    const long = new Date(projects[projects.length-1].createdAt) -start
+    const toPersent = (date) => {
+        return ((date - start)/long)*1000
+    }
+    let xp = 0;
+    const maxXp = projects.reduce((sum, item) => sum + item.xp, 0);
+    let res ="";
+    let last ="";
+    let points = "0,0";
+    projects.forEach((project) => {
+        xp += project.xp
+        let x = toPersent(new Date(project.createdAt));
+        let y = (xp/maxXp)*1000;
+        console.log(x,y ,project,maxXp,(xp/maxXp),xp,(xp/maxXp)*350)
+        last += `<circle  cx="${x}" cy="${y}" r="5" fill="red" ><title>${project.project} : ${project.xp  }  PUSHED AT: ${new Date(project.createdAt).toLocaleDateString()}</title></circle>\n`;
+        points +=` ${x},${y} `
+    });
+    points += "1000,0"
+    res += `<polygon id="skill-polygon" points="${points}" fill="rgba(0,123,255,0.5)" stroke="#007bff" stroke-width="5" />\n`
+    displayof_module_nodata.innerHTML = ""
+    displayof_module_number_5.innerHTML = Math.round(maxXp/5)>1000?Math.round((maxXp/5)/1000)+ " KB":Math.round(maxXp/5) +" B";
+    displayof_module_number_4.innerHTML = Math.round(maxXp/4)>1000?Math.round((maxXp/4)/1000)+ " KB":Math.round(maxXp/4) +" B";
+    displayof_module_number_3.innerHTML = Math.round(maxXp/3)>1000?Math.round((maxXp/3)/1000)+ " KB":Math.round(maxXp/3) +" B";
+    displayof_module_number_2.innerHTML = Math.round(maxXp/2)>1000?Math.round((maxXp/2)/1000)+ " KB":Math.round(maxXp/2) +" B";
+    displayof_module_number_1.innerHTML = Math.round(maxXp/1)>1000?Math.round((maxXp/1)/1000)+ " KB":Math.round(maxXp/1) +" B";
+    displayof_module_number_0.innerHTML = "0 B";
+    return res+last
+}
+
 async function loadPage() {
     const data = await new Data().init();
     const profile = await new Profile().init();
     console.log("data", data)
     console.log("profile", profile)
-    window.skills = data.skills
+
+    displayof_module_graph.innerHTML = module(data.projects)
+    window.projects = data.projects
     if (data.skills < 10 ) {
         graph_display.innerTML = renderGraph(skills)
     } else {
